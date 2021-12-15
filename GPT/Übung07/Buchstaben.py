979560c4776efb0411b398471f670a37c68319de
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2021-12-15 21:30:59 +0100

    solve 7.3
    solution is limited to searching for ASCII characters

---
+++ GPT/Übung07/Buchstaben.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    # receive a letter as a command line argument
+    target_letter = sys.argv[1].lower()
+
+    # search for the letter in the input, increment the counter if found
+    counter = 0
+    for line in sys.stdin:
+        for letter in line:
+            if letter.lower() == target_letter:
+                counter += 1
+
+    sys.stdout.write(f'Im vorliegenden Text gibt es den Buchstaben \'{target_letter}\' so oft: {counter} mal.\n')
+
+
+if __name__ == "__main__":
+    main()
