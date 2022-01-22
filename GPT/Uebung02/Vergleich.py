105bb266a5a47ab2ee73d635a53c2bd00babf56d
Sergej Khramov <khramov.sergej@gmail.com>
2022-01-22 23:19:31 +0100

    solve 2.2

---
+++ GPT/Uebung02/Vergleich.py
@@ -0,0 +1,17 @@
+import sys
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+if (int(a) == int(b) == int(c)):
+    print("Gleich")
+
+if (int(a) < int(b) < int(c)):
+    print("Aufsteigend sortiert")
+
+if (int(a) > int(b) > int(c)):
+    print("Absteigend sortiert")
+
+if not(int(a) == int(b) == int(c)) and not(int(a) < int(b) < int(c)) and not(int(a) > int(b) > int(c)):
+    print("Unsortiert")
