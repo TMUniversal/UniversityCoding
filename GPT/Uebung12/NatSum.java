ee779d792c210d5703dc61f29f4c53b8c1fc18ef
DerFred3 <Der-Fred3@web.de>
2022-01-30 22:23:05 +0100

    Auch hier Rechenweg

--- GPT/Uebung12/NatSum.java
+++ GPT/Uebung12/NatSum.java
@@ -1,11 +1,18 @@
 public class NatSum {
     public static void main(String[] args) {
         int sum = 0;
-
+        String ret = "";
         for (int i = 1; i < Integer.parseInt(args[0]) + 1; i++) {
+            if (ret != "") {
+                ret += " + " + String.valueOf(i);
+            } else {
+                ret += String.valueOf(i);
+            }
             sum += i;
         }
 
-        System.out.println(sum);
+        ret += " = " + String.valueOf(sum);
+
+        System.out.println(ret);
     }
 }
