8c131f4ecc54f10fa39bb71d935ddb567c78bc76
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2022-02-02 21:18:30 +0100

    shorten 12.2

--- GPT/Uebung12/Multi.java
+++ GPT/Uebung12/Multi.java
@@ -1,11 +1,5 @@
 public class Multi {
     public static void main(String[] args) {
-        String ret = "";
-
-        for (int i = 0; i < Integer.parseInt(args[1]); i++) {
-            ret += args[0];
-        }
-
-        System.out.println(ret);
+        System.out.println(args[0].repeat(Integer.parseInt(args[1])));
     }
 }
